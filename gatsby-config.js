26a30cb0b7575a4447071f5dc4dad9e139212f0b
tricinel <bogdan@bogdanlazar.com>
2018-03-13 13:19:22 +0100

    Add favicon and support for manifest.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,59 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Frontwerk Docs',
+        short_name: 'Frontwerk',
+        start_url: '/',
+        background_color: '#10bc9b',
+        theme_color: '#34485e',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/favicons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: '/favicons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }
   ]
 };

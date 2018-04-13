d8121250725ff948a5dcbb27b9992e67610d4c4f
tricinel <bogdan@bogdanlazar.com>
2018-04-13 12:56:09 +0200

    Add accessibility improvements to svg icons

--- src/components/Sidebar/CloseIcon.js
+++ src/components/Sidebar/CloseIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MenuIcon = ({ size = 20 }) => (
+const CloseIcon = ({ size = 20 }) => (
   <svg
     viewBox="0 0 248 248"
     version="1.1"
@@ -8,7 +8,9 @@ const MenuIcon = ({ size = 20 }) => (
     y="0px"
     width={size}
     height={size}
+    aria-labelledby="svgtitle-close-menu"
   >
+    <title id="svgtitle-close-menu">Close menu</title>
     <g transform="translate(-141.000000, -90.000000)">
       <polygon
         points="389 114.8 364.2 90 265 189.2 165.8 90 141 114.8 240.2 214 141 313.2 165.8 338 265 238.8 364.2 338 389 313.2 289.8 214"
@@ -18,4 +20,4 @@ const MenuIcon = ({ size = 20 }) => (
   </svg>
 );
 
-export default MenuIcon;
+export default CloseIcon;
